ab11e9b30849ecd54178fa005e1a4e430c433e7d
Jason Kottler <jason.kottler@skyscanner.net>
2018-03-23 17:21:37 +0000

    Generate all the dice to check them out

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Die from './components/die';
+
+function dice() {
+  // return [
+  //   <Die value="1" type="gear" />,
+  //   <Die value="1" type="base" />,
+  //   <Die value="1" type="skill" />,
+  //   <Die value="2" type="gear" />,
+  //   <Die value="6" type="gear" />,
+  // ];
+  let values = Array.from(new Array(6), (x, i) => i + 1);
+  let types = ['base', 'gear', 'skill'];
+
+  return values.map(v => types.map(t => <Die value={v} type={t} />));
+
+  // return values.map(v => <Die value={v} type="gear" />);
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to MYZ Roller</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">{dice()}</div>
       </div>
     );
   }

a1cd88f3f899cb2540915a363149dc53ffbd515e
Jason Kottler <jason.kottler@skyscanner.net>
2018-04-05 23:22:33 +0100

    Switch to range inputs with die icons

--- src/components/numberSelector.jsx
+++ src/components/numberSelector.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import Die from './die';
 // import { render } from 'react-dom';
+
+const style = {
+  fontSize: 'xx-large',
+};
 
 class NumberSelector extends Component {
   constructor(props) {
@@ -12,17 +17,22 @@ class NumberSelector extends Component {
 
   render() {
     return (
-      <div className="input-field">
-        <div>{this.props.name}</div>
-        <input
-          placeholder={this.props.name}
-          id={this.props.name}
-          type="number"
-          min="0"
-          max="20"
-          value={this.props.value}
-          onChange={this.handleChange}
-        />
+      <div className="row">
+        <div className="input-field">
+          <Die className="col s2" value="6" type={this.props.name} />
+          <div style={style} className="valign-wrapper col s1 right">
+            {this.props.value}
+          </div>
+          <input
+            placeholder={this.props.name}
+            id={this.props.name}
+            type="range"
+            min="0"
+            max="8"
+            value={this.props.value}
+            onChange={this.handleChange}
+          />
+        </div>
       </div>
     );
   }

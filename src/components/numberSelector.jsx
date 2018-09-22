6a1eafae6e898ab220d408af22a19e6907a2500d
Jason Kottler <jason@kottlerfamily.com>
2018-09-22 17:53:07 -0600

    Remove unused import and apply standard js fixes

--- src/components/numberSelector.jsx
+++ src/components/numberSelector.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import Die from './die';
-import IncrementButton from './incrementButton';
+import React, { Component } from 'react'
+import IncrementButton from './incrementButton'
 
 const style = {
   fontSize: 'xx-large',
@@ -8,11 +7,11 @@ const style = {
 }
 
 class NumberSelector extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.handleChange = e => {
-      this.props.onChange(this.props.name, e.target.value);
+      this.props.onChange(this.props.name, e.target.value)
     };
 
     this.handleIncrement = e => {
@@ -20,49 +19,49 @@ class NumberSelector extends Component {
         this.props.onChange(
           this.props.name,
           parseInt(this.props.value, 10) + 1
-        );
+        )
       }
-    };
+    }
 
     this.handleDecrement = e => {
       if (this.props.value > 0) {
         this.props.onChange(
           this.props.name,
           parseInt(this.props.value, 10) - 1
-        );
+        )
       }
-    };
+    }
   }
 
-  render() {
+  render () {
     return (
-      <div className="row">
-        <div className="input-field">
+      <div className='row'>
+        <div className='input-field'>
           <IncrementButton
-            label="-"
+            label='-'
             type={this.props.name}
             onClick={this.handleDecrement}
           />
           <span style={style}>{this.props.value}</span>
           <IncrementButton
-            label="+"
+            label='+'
             type={this.props.name}
             onClick={this.handleIncrement}
           />
           <input
             placeholder={this.props.name}
             id={this.props.name}
-            type="range"
-            min="0"
-            max="8"
+            type='range'
+            min='0'
+            max='8'
             value={this.props.value}
             onChange={this.handleChange}
           />
-          
+
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default NumberSelector;
+export default NumberSelector

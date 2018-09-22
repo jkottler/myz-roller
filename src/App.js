7de74f246f593557d14ef2b70b821e1c414a04ea
Jason Kottler <jason@kottlerfamily.com>
2018-09-22 17:51:00 -0600

    Add event tracking for roll and push.
    Also apply standard js fixes.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,55 @@
-import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css';
-import React, { Component } from 'react';
-import logo from './d6-6-glow.svg';
-import Random from 'random-js';
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css'
+import React, { Component } from 'react'
+import logo from './d6-6-glow.svg'
+import Random from 'random-js'
+import ReactGA from 'react-ga'
 
-import './App.css';
+import './App.css'
 
-import Die from './components/die';
-import SelectorForm from './components/selectorForm';
-import DiceDisplay from './components/diceDisplay';
+import Die from './components/die'
+import SelectorForm from './components/selectorForm'
+import DiceDisplay from './components/diceDisplay'
 
 // init materialize
-M.AutoInit();
+M.AutoInit()
+
+// init google tracking
+ReactGA.initialize('UA-104501320-2')
 
 // create a Mersenne Twister-19937 that is auto-seeded based on time and other random values
-var engine = Random.engines.mt19937().autoSeed();
-var distribution = Random.integer(1, 6);
+var engine = Random.engines.mt19937().autoSeed()
+var distribution = Random.integer(1, 6)
 
-function roll() {
-  return distribution(engine);
+function roll () {
+  return distribution(engine)
 }
 
-function rollDice(baseCount, skillCount, gearCount) {
-  let result = [];
+function rollDice (baseCount, skillCount, gearCount) {
+  let result = []
 
-  let index = 0;
+  let index = 0
 
   for (let i = 0; i < baseCount; i++) {
-    result.push(<Die value={roll()} type="base" key={index} />);
-    index++;
+    result.push(<Die value={roll()} type='base' key={index} />)
+    index++
   }
 
   for (let i = 0; i < skillCount; i++) {
-    result.push(<Die value={roll()} type="skill" key={index} />);
-    index++;
+    result.push(<Die value={roll()} type='skill' key={index} />)
+    index++
   }
 
   for (let i = 0; i < gearCount; i++) {
-    result.push(<Die value={roll()} type="gear" key={index} />);
-    index++;
+    result.push(<Die value={roll()} type='gear' key={index} />)
+    index++
   }
-  return result;
+  return result
 }
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = {
       baseCount: 0,
@@ -53,57 +57,65 @@ class App extends Component {
       gearCount: 0,
       dice: rollDice(0, 0, 0),
       pushDisabled: 'disabled',
-      rollDisabled: 'disabled',
-    };
+      rollDisabled: 'disabled'
+    }
 
     this.handleRoll = (e) => {
-      let { baseCount, skillCount, gearCount } = e;
-      baseCount = parseInt(baseCount, 10);
-      skillCount = parseInt(skillCount, 10);
-      gearCount = parseInt(gearCount, 10);
+      let { baseCount, skillCount, gearCount } = e
+      baseCount = parseInt(baseCount, 10)
+      skillCount = parseInt(skillCount, 10)
+      gearCount = parseInt(gearCount, 10)
       this.setState({
         baseCount,
         skillCount,
         gearCount,
         dice: rollDice(baseCount, skillCount, gearCount),
-        pushDisabled: '',
-      });
-    };
+        pushDisabled: ''
+      })
+      ReactGA.event({
+        category: 'Dice',
+        action: 'roll'
+      })
+    }
 
     this.handlePush = (e) => {
-      let newDice = [];
+      let newDice = []
       for (let die of this.state.dice) {
         if (
           die.props.value === 6 ||
           (die.props.value === 1 && die.props.type !== 'skill')
         ) {
           // keep symbol dice
-          newDice.push(die);
+          newDice.push(die)
         } else {
           // reroll others
           newDice.push(
             <Die value={roll()} type={die.props.type} key={die.key} />
-          );
+          )
         }
       }
-      this.setState({ dice: newDice, pushDisabled: 'disabled' });
+      this.setState({ dice: newDice, pushDisabled: 'disabled' })
     };
+    ReactGA.event({
+      category: 'Dice',
+      action: 'roll'
+    })
   }
 
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo hide-on-small-only" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo hide-on-small-only' alt='logo' />
           <div>
-            <h1 className="App-title">Welcome to MYZ Roller</h1>
+            <h1 className='App-title'>Welcome to MYZ Roller</h1>
           </div>
         </header>
 
         <main>
-          <div className="container">
-            <div className="row">
-              <div className="col s4">
+          <div className='container'>
+            <div className='row'>
+              <div className='col s4'>
                 <SelectorForm
                   baseCount={this.state.baseCount}
                   skillCount={this.state.skillCount}
@@ -114,41 +126,41 @@ class App extends Component {
                   rollDisabled={this.state.rollDisabled}
                 />
               </div>
-              <div className="col s8">
+              <div className='col s8'>
                 <DiceDisplay dice={this.state.dice} />
               </div>
             </div>
           </div>
         </main>
 
-        <footer className="page-footer">
-          <div className="container">
-            <div className="row">
-              <div className="col s12">
-                <div className="col s4">
+        <footer className='page-footer'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col s12'>
+                <div className='col s4'>
                   A dice roller for:{' '}
                   <a
-                    href="https://www.modiphius.com/mutant.html"
-                    rel="noopener noreferrer"
-                    target="_blank">
+                    href='https://www.modiphius.com/mutant.html'
+                    rel='noopener noreferrer'
+                    target='_blank'>
                     Mutant: Year Zero
                   </a>
                 </div>
-                <div className="col s4">
+                <div className='col s4'>
                   Icons from:{' '}
                   <a
-                    href="http://game-icons.net"
-                    rel="noopener noreferrer"
-                    target="_blank">
+                    href='http://game-icons.net'
+                    rel='noopener noreferrer'
+                    target='_blank'>
                     game-icons.net
                   </a>
                 </div>
-                <div className="col s4">
+                <div className='col s4'>
                   Contribute on:{' '}
                   <a
-                    href="https://github.com/jkottler/myz-roller"
-                    rel="noopener noreferrer"
-                    target="_blank">
+                    href='https://github.com/jkottler/myz-roller'
+                    rel='noopener noreferrer'
+                    target='_blank'>
                     Github
                   </a>
                 </div>
@@ -157,8 +169,8 @@ class App extends Component {
           </div>
         </footer>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App

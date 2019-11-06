d27b7d9ca782570e3d20675c8f9ac18f52959b42
Jason Kottler <jkottler@users.noreply.github.com>
2019-11-06 11:05:11 -0700

    Updates

---
+++ precache-manifest.9fe0829f7de9eae06c37fc66babceb54.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3563a28aabdc203fdf6cf6f1e1a6a45",
+    "url": "/myz-roller/index.html"
+  },
+  {
+    "revision": "30b22ed652135128d303",
+    "url": "/myz-roller/static/css/2.cd064b02.chunk.css"
+  },
+  {
+    "revision": "13b52ce80edce7a917a1",
+    "url": "/myz-roller/static/css/main.cf94fbb5.chunk.css"
+  },
+  {
+    "revision": "30b22ed652135128d303",
+    "url": "/myz-roller/static/js/2.1c777b41.chunk.js"
+  },
+  {
+    "revision": "13b52ce80edce7a917a1",
+    "url": "/myz-roller/static/js/main.607f9e70.chunk.js"
+  },
+  {
+    "revision": "0e6a4f12b58a1369b440",
+    "url": "/myz-roller/static/js/runtime-main.81ebf0c2.js"
+  },
+  {
+    "revision": "8f1481114fd3ccafb57f5b93ad247f32",
+    "url": "/myz-roller/static/media/biohazard.8f148111.svg"
+  },
+  {
+    "revision": "bd3f6fbffb285691d1335e197cdb50a4",
+    "url": "/myz-roller/static/media/corner-explosion.bd3f6fbf.svg"
+  },
+  {
+    "revision": "7dd09130bb66949536109bc9ba7c39ff",
+    "url": "/myz-roller/static/media/d6-6-glow.7dd09130.svg"
+  },
+  {
+    "revision": "9b0f30b3070e1239a1e31539f9cb2f77",
+    "url": "/myz-roller/static/media/radioactive-black.9b0f30b3.svg"
+  },
+  {
+    "revision": "e2e3c84444c7e2fd5243e8e8f77b5df2",
+    "url": "/myz-roller/static/media/radioactive-white.e2e3c844.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myz-roller/precache-manifest.e1c3026a6fa5183b9c5e7a5d4587e978.js"
+  "/myz-roller/precache-manifest.9fe0829f7de9eae06c37fc66babceb54.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myz-roller/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myz-roller/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
